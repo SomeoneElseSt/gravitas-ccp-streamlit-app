e72f693f987e1428bc990495ed7bfc0c2b05e88d
Stiven <Strnmrtnz@icloud.com>
2026-03-06 06:22:48 +0530

    chore: fixed extra str lose

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ from google.genai import types
 
 
 # Create Streamlit app layout
-st.title("Gravitas: Land Surface Temperature And Urban Heat Index Analysis 🛰️ 🌡️ "")
+st.title("Gravitas: Land Surface Temperature And Urban Heat Index Analysis 🛰️ 🌡️ ")
 
 # Streamlit Config
 st.set_page_config(
